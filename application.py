0b3c7f051c3374b763d458e4d1e99849acf4fdee
zhengxiaoyao0716 <1499383852@qq.com>
2016-04-09 21:44:20 +0800

    修改逻辑，增加Notice、news

--- application.py
+++ application.py
@@ -1,14 +1,31 @@
 #coding=UTF-8
 
 from flask import Flask, jsonify
-from data_util import Data
+import data_util
 
 app = Flask(__name__)
 
-@app.route('/data')
-def get_data():
-    return jsonify(Data().data)
-    
 
+#####################Util#####################
+##包装返回体
+def pack_responce(success, content, message = None):
+    return jsonify({'success': success, 'content': content, 'message': message})
+
+
+#####################Api#####################
+@app.route('/data/<page>')
+def data(page):
+    crawler = data_util.fetch(page)
+    if crawler:
+        return pack_responce(True, crawler.data)
+    else:
+        return pack_responce(False, None, 'Unkown page.')
+    
+@app.route('/view/<href>')
+def view(href):
+    return pack_responce(True, href)
+    
+    
+#####################Run#####################
 if __name__ == '__main__':
     app.run(debug = True, port = 5100)
--- data_util.py
+++ data_util.py
@@ -1,9 +1,12 @@
 #coding=UTF-8
 
-import time, requests
+import abc, time, requests
 from bs4 import BeautifulSoup
-class Data(object):
+#数据基类
+class BaseData(object):
+    __metaclass__ = abc.ABCMeta
     #报警，比如源页面改版导致解析失败
+    @staticmethod
     def alarm(msg):
         pass
         
@@ -16,7 +19,7 @@ class Data(object):
             if not tds:
                 continue
             if len(tds) != 3:
-                alarm(u'3个td描述一则记录的规律失效')
+                BaseData.alarm(u'3个td描述一则记录的规律失效')
             record = {
                 'href': tds[1].a['href'],
                 'text': tds[1].text,
@@ -26,23 +29,36 @@ class Data(object):
         return result
         
     last_time = 0
-    cache_data = {}
-    def __init__(self):
+    def __init__(self, url):
         #降频
-        if time.time() - Data.last_time < 30:
-            self.data = Data.cache_data
+        now_time = time.time()
+        if now_time - self.__class__.last_time < 30:
+            self.data = self.__class__.data
             return
-        Data.last_time = time.time()
-        print('aaa')
-        
-        html = requests.get('http://oice.ustb.edu.cn/xinwenzhongxin/').text
+        self.__class__.last_time = now_time
+               
+        html = requests.get(url).text
         soup = BeautifulSoup(html, "html.parser")
+        self.__class__.data = self.data = self.parse(soup)
+        
+    #解析
+    @abc.abstractmethod
+    def parse(self, soup):
+        pass
+        
+        
+#主界面
+class HomeData(BaseData):
+    def __init__(self):
+        super(HomeData, self).__init__('http://oice.ustb.edu.cn/xinwenzhongxin/')
+        
+    def parse(self, soup):
         info_tables = soup.select('.y-13')
         if not info_tables:
-            alarm(u'.y-13的table展示信息')
-        Data.cache_data = self.data = {
-            'notice': Data.parse_table(info_tables[0]),
-            'news': Data.parse_table(info_tables[1])
+            BaseData.alarm(u'.y-13的table展示信息')
+        return {
+            'notice': BaseData.parse_table(info_tables[0]),
+            'news': BaseData.parse_table(info_tables[1])
         }
         
     #公告通知
@@ -52,3 +68,40 @@ class Data(object):
     #新闻速递
     def news(self):
         return self.data['news']
+        
+        
+#公告
+class NoticeData(BaseData):
+    def __init__(self):
+        super(NoticeData, self).__init__('http://oice.ustb.edu.cn/xinwenzhongxin/gonggaotongzhi/')
+        
+    def parse(self, soup):
+        info_tables = soup.select('.y-13')
+        if not info_tables:
+            BaseData.alarm(u'.y-13的table展示信息')
+        return {'records': BaseData.parse_table(info_tables[0])}
+        
+        
+#新闻
+class NewsData(BaseData):
+    def __init__(self):
+        super(NewsData, self).__init__('http://oice.ustb.edu.cn/xinwenzhongxin/xinwensudi/')
+        
+    def parse(self, soup):
+        info_tables = soup.select('.y-13')
+        if not info_tables:
+            BaseData.alarm(u'.y-13的table展示信息')
+        return {'records': BaseData.parse_table(info_tables[0])}
+        
+        
+#holder
+dataMap = {
+    'home': HomeData(),
+    'notice': NoticeData(),
+    'news': NewsData()
+}
+def fetch(page):
+    if page in dataMap:
+        return dataMap[page]
+    else:
+        return False
